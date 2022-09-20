cefd8604305bc3fa28e179160a14f0a38ac806cb
Rosalba Vázquez <rosalbamusician@gmail.com>
2022-09-20 08:30:14 -0600

    Every function on its place

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import { getAuth, updateProfile } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js'
+import { getAuth} from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js'
 import { app } from './config.js';
 
 export const auth = getAuth(app);
